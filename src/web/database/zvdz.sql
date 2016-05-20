7498084c94630a91fd923d2676d2ec4bc977714f
Roel van Lisdonk <roelvanlisdonk@live.com>
2016-05-20 16:10:31 +0200

    initial commit

---
+++ src/web/database/zvdz.sql
@@ -0,0 +1,22 @@
+﻿
+-- Show user on name
+select	l.* 
+from	[Login] l with (nolock)
+where	Gebruikersnaam = 'ada5@ada.nl'
+
+-- Show user on id
+select	l.* 
+from	[Login] l with (nolock)
+where	l.Id = 1711
+
+-- Reset failed login count
+update	l
+set		l.failedLoginCount = 0
+from	[Login] l with (nolock)
+where	l.Id = 1711
+
+-- Reset "algemene voorwaarden akkoord"
+update	l
+set		l.ApprovedConditions = null
+from	[login] l with (nolock)
+where	l.Id = 1711
