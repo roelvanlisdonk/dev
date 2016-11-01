164cd949217dda43f861098835f6495e3e349930
Roel van Lisdonk <roelvanlisdonk@live.com>
2016-11-01 22:29:15 +0100

    x

--- am/main.ts
+++ am/main.ts
@@ -1,36 +1,13 @@
 import { div, getVirtualDom, IVirtualDomNode, span } from './services/virtual.dom'
 import { render } from './services/dom'
+import { IStoreRecord, IStoreField } from './services/store'
 
 const body = document.querySelector('body');
 
 const bodyVirtualDom = getVirtualDom(body);
-bodyVirtualDom.childNodes = [
-    div({ text: 'my test'})
-];
-
-render(body, bodyVirtualDom);
 
 
-
-// const childs = div(
-//     div(),
-//     span()
-// );
-// div({ text: 'Dit is mijn tekst' })
-
-//      const result = 
-//      div({ class: [container], childs: [
-//          div({ class: [label], text: resources.infoOnCars, deps: [])
-//          button({ class: [action-button], text: resources.save, onclick: fn, deps: [])
-//      ], deps: []});
-
-
-// Update dom
-// const body = document.createElement('body');
-// const element = document.createElement('div');
-// element.appendChild(document.createTextNode('Test !!!'));
-// body.appendChild(element);
-
-// const html = document.childNodes[1];
-// html.appendChild(body);
-
+class User I {
+    id: string;
+    readonly typeId
+}
--- am/services/store.ts
+++ am/services/store.ts
@@ -1,36 +1,149 @@
 
-export interface IStoreArray extends IStoreObject {
-    deps: Array<IStoreDepenendcy>;
-    id: string; // GUID
+const objectIndex: any = {}; // Array<IStoreObject>
+const typeIndex: any = {}; // Array<IStoreType>
+
+// To support renaming of types and fields, we expect objects in the store to be of type IStoreObject
+// Fields of a IStoreObject can only be of type IStoreField (except for the readonly fields "id" and "typeId".
+
+// This function converts the given type, so fields can be accessed fast.
+export function addType() {
+ 
 }
 
-export interface IStoreDepenendcy {
-    deps: Array<IStoreDepenendcy>;
+
+function cloneSimpleType(obj: any): any {
+    let copy: any;
+
+    // Handle boolean, number, string, null and undefined.
+    if (null == obj || "object" != typeof obj) { 
+        return copy;
+    };
+
+    // Handle Date
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    throw new Error("Unable to clone simple type.");
 }
 
-export interface IStoreField extends IStoreObject {
-	deps: Array<IStoreObject>;
-	fieldTypeId: string; // GUID
-    id: string; // GUID
-	recordId: string; // GUID
-    value: boolean | Date | number | string;
+export function get<T>(id: string): T {
+    const obj: any = objectIndex[id];
+
+    let copy: any; 
+
+    // Handle Object
+    if (obj instanceof Object) {
+        copy = {
+            id: obj.id,
+            typeId: obj.typeId
+        };
+
+        for (let propName in obj) {
+            if (obj.hasOwnProperty(propName)) {
+                const propValue = obj[propName];
+
+                // Only handle IStoreField.
+                if("object" === typeof propValue && propValue.fieldId) {
+                    // TODO: lookup correct field.
+                    const fieldCopy = {
+
+                    };
+
+                    // Handle IStoreField.value is of type 'Array'.
+                    if (propValue.value instanceof Array) {
+                        let guids: Array<string> = [];
+                        for (let i = 0, len = propValue.length; i < len; i++) {
+                            guids[i] = cloneSimpleType(propValue[i]); // Array items should be of simple types!.
+                        }
+
+                        // TODO: lookup correct field.
+                        copy[propName] = guids;
+                    }
+                }
+            }
+        }
+        return copy;
+    }
+
+    throw new Error("Unable to copy obj! Its type isn't supported.");
 }
 
-export interface IStoreFnDepenendcy extends IStoreDepenendcy {
-    deps: Array<IStoreDepenendcy>;
-    fn: (...deps: Array<IStoreDepenendcy>) => any;
+
+
+export interface IStoreType {
+    readonly typeId: string; // GUID
 }
 
-export interface IStoreObject extends IStoreDepenendcy {
-    deps: Array<IStoreDepenendcy>;
-    id: string; // GUID
+export interface IStoreObject extends IStoreType {
+    readonly id: string; // GUID
 }
 
-export interface IStoreRecord extends IStoreObject {
-	deps: Array<IStoreObject>;
-    id: string; // GUID
-	recordTypeId: string; // GUID
-    // Field1
-    // Feild2
-    // ...
+export interface IStoreField {
+    readonly fieldId: string; // GUID
+    value: boolean | Date | number | string | Array<boolean | Date | number | string>;
 }
+
+export interface IStoreBooleanField extends IStoreField {
+    value: boolean;
+}
+
+export interface IStoreDateField extends IStoreField {
+    value: Date;
+}
+
+export interface IStoreNumberField extends IStoreField {
+    value: number;
+}
+
+export interface IStoreStringField extends IStoreField {
+    value: string;
+}
+
+export interface IStoreBooleanArrayField extends IStoreField {
+    value: Array<boolean>;
+}
+
+export interface IStoreDateArrayField extends IStoreField {
+    value: Array<Date>;
+}
+
+export interface IStoreNumberArrayField extends IStoreField {
+    value: Array<number>;
+}
+
+export interface IStoreStringArrayField extends IStoreField {
+    value: Array<string>;
+}
+
+
+
+// export interface IStoreArray extends IStoreObject {
+//     id: string;
+//     [position: number]: IStoreObject;
+// }
+
+// export interface IStoreDepenendcy {
+//     deps: Array<IStoreDepenendcy>;
+// }
+
+// export interface IStoreField extends IStoreObject {
+// 	fieldTypeId: string;
+//     id: string;
+// 	recordId: string;
+//     value: boolean | Date | number | string;
+// }
+
+// export interface IStoreFnDepenendcy extends IStoreDepenendcy {
+//     deps: Array<IStoreDepenendcy>;
+//     fn: (...deps: Array<IStoreDepenendcy>) => any;
+// }
+
+
+
+// export interface IStoreRecord extends IStoreObject {
+//     id: string;
+// 	recordTypeId: string;
+// }
