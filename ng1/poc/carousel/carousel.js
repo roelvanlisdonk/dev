fbb1aecd71b83f67debdcb0d77a3058acb0db236
Roel van Lisdonk <roelvanlisdonk@live.com>
2016-11-16 23:09:02 +0100

    x

--- ng1/poc/carousel/carousel.js
+++ ng1/poc/carousel/carousel.js
@@ -2,9 +2,10 @@ var poc;
 (function (poc) {
     'use strict';
     var _slideWidth = 1000;
-    angular.module('poc').directive('carousel', [function () { return new CarouselDirective(); }]);
+    angular.module('poc').directive('carousel', ['$timeout', function ($timeout) { return new CarouselDirective($timeout); }]);
     var CarouselDirective = (function () {
-        function CarouselDirective() {
+        function CarouselDirective($timeout) {
+            this.$timeout = $timeout;
             this.restrict = 'EA';
             this.scope = {
                 options: '=?carousel'
@@ -21,55 +22,85 @@ var poc;
             if (!$scope.options) {
                 $scope.options = getStubOptions();
             }
-            setTransform(0);
+            var items = $scope.options.items;
+            var slides = [];
+            var totalItemCount = items.length;
+            slides.unshift(items[totalItemCount - 1]);
+            for (var i = 0, length_1 = totalItemCount; i < length_1; i++) {
+                slides.push(items[i]);
+            }
+            slides.push(items[0]);
+            $scope.slides = slides;
+            $scope.currentPagerItemIndex = 0;
+            $scope.currentSlideIndex = 1;
+            setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             setSlidesWidth();
             function setSlidesWidth() {
-                $scope.slidesWidth = $scope.options.items.length * _slideWidth + "px";
+                $scope.slidesWidth = $scope.slides.length * _slideWidth + "px";
             }
             function onNextClick() {
-                var total = $scope.options.items.length;
+                var total = $scope.slides.length;
                 if (total === 0) {
                     return;
                 }
-                var current = $scope.options.currentItemIndex || 0;
+                var current = $scope.currentSlideIndex;
                 var next = current + 1;
                 if (next === total) {
                     next = 0;
                 }
-                $scope.options.currentItemIndex = next;
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                $scope.currentSlideIndex = next;
+                $scope.currentPagerItemIndex = next + 1;
+                setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             }
             function onPagerItemClick(index) {
-                $scope.options.currentItemIndex = index;
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                $scope.currentPagerItemIndex = index;
+                $scope.currentSlideIndex = index + 1;
+                setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             }
             function onPreviousClick() {
-                var total = $scope.options.items.length;
+                var total = $scope.slides.length;
                 if (total === 0) {
                     return;
                 }
-                var current = $scope.options.currentItemIndex || 0;
-                var previous = current - 1;
-                if (previous < 0) {
-                    previous = total - 1;
+                var current = $scope.currentSlideIndex;
+                if (current === 0) {
+                    $scope.transitionEnabled = false;
+                    var offset = 0 - ((total - 2) * _slideWidth);
+                    $scope.transform = "translateX(" + offset + "px)";
+                    self.$timeout(function () {
+                        $scope.transitionEnabled = true;
+                        self.$timeout(function () {
+                            setTransform(0 - ((total - 3) * _slideWidth));
+                            $scope.currentSlideIndex = total - 3;
+                            $scope.currentPagerItemIndex = total - 4;
+                        }, 100);
+                    }, 0);
                 }
-                $scope.options.currentItemIndex = previous;
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                else {
+                    var previous = current - 1;
+                    if (previous < 0) {
+                        previous = total - 1;
+                    }
+                    $scope.currentSlideIndex = previous;
+                    $scope.currentPagerItemIndex = previous - 1;
+                    setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
+                }
             }
             function setTransform(offSet) {
+                $scope.transitionEnabled = true;
                 $scope.transform = "translateX(" + offSet + "px)";
             }
         };
         return CarouselDirective;
     }());
     function getStubOptions() {
+        var items = [
+            { title: 'title 1', background: 'url(/ng1/poc/carousel/img1_small.png)' },
+            { title: 'title 2', background: 'url(/ng1/poc/carousel/img2_small.png)' },
+            { title: 'title 3', background: 'url(/ng1/poc/carousel/img3_small.png)' }
+        ];
         var options = {
-            currentItemIndex: 0,
-            items: [
-                { title: 'title 1', background: 'url(/App/Core/Directives/Carousel/img1_small.png)' },
-                { title: 'title 2', background: 'url(/App/Core/Directives/Carousel/img2_small.png)' },
-                { title: 'title 3', background: 'url(/App/Core/Directives/Carousel/img3_small.png)' }
-            ]
+            items: items
         };
         return options;
     }
--- ng1/poc/carousel/carousel.ts
+++ ng1/poc/carousel/carousel.ts
@@ -2,7 +2,7 @@ namespace poc {
     'use strict';  
     const _slideWidth = 1000;
 
-    angular.module('poc').directive('carousel', [() => new CarouselDirective()]);
+    angular.module('poc').directive('carousel', ['$timeout', ($timeout) => new CarouselDirective($timeout)]);
 
     class CarouselDirective implements ng.IDirective {
         public link: ($scope: ICarouselScope, $element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
@@ -12,7 +12,7 @@ namespace poc {
         };
         public templateUrl = '/ng1/poc/carousel/carousel.html';
 
-        constructor() {
+        constructor(public $timeout: ng.ITimeoutService) {
             const self: CarouselDirective = this;
 
             self.link = self.unboundLink.bind(self);
@@ -27,63 +27,101 @@ namespace poc {
             if (!$scope.options) {
                 $scope.options = getStubOptions();
             }
-            setTransform(0);
+            const items = $scope.options.items;
+            const slides: Array<ICarouselItem> = [];
+            const totalItemCount = items.length;
+            slides.unshift(items[totalItemCount - 1]);
+            for(let i = 0, length = totalItemCount; i < length; i++) {
+                slides.push(items[i]);
+            }
+            slides.push(items[0]);
+            $scope.slides = slides;
+            $scope.currentPagerItemIndex = 0;
+            $scope.currentSlideIndex = 1;
+
+            setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             setSlidesWidth();
 
             function setSlidesWidth() {
-                $scope.slidesWidth = `${$scope.options.items.length * _slideWidth}px`;
+                $scope.slidesWidth = `${$scope.slides.length * _slideWidth}px`;
             }
 
             function onNextClick() {
-                const total = $scope.options.items.length;
+                const total = $scope.slides.length;
                 if (total === 0) { return; }
-                const current = $scope.options.currentItemIndex || 0;
+                const current = $scope.currentSlideIndex;
 
                 let next = current + 1;
                 if (next === total) {
                     next = 0;
                 }
-                $scope.options.currentItemIndex = next;
+                $scope.currentSlideIndex = next;
+                $scope.currentPagerItemIndex = next + 1;
 
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             }
 
             function onPagerItemClick(index: number) {
-                $scope.options.currentItemIndex = index;
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                $scope.currentPagerItemIndex = index;
+                $scope.currentSlideIndex = index + 1;
+                setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
             }
 
             function onPreviousClick() {
-                const total = $scope.options.items.length;
+                const total = $scope.slides.length;
                 if (total === 0) { return; }
-                const current = $scope.options.currentItemIndex || 0;
+                const current = $scope.currentSlideIndex;
+                if(current === 0) {
+                    
+                    $scope.transitionEnabled = false;
+                    const offset = 0 - ((total - 2)  * _slideWidth);
+                    $scope.transform = `translateX(${offset}px)`;
 
-                let previous = current - 1;
-                if (previous < 0) {
-                    previous = total - 1;
+                    self.$timeout(()=>{
+                        $scope.transitionEnabled = true;
+                         self.$timeout(()=>{
+                            setTransform(0 - ((total - 3)  * _slideWidth));
+                            $scope.currentSlideIndex = total - 3;
+                            $scope.currentPagerItemIndex = total - 4;
+                         }, 100);
+                    }, 0);
+                    
+                   
+                } else {
+                    let previous = current - 1;
+                    if (previous < 0) {
+                        previous = total - 1;
+                    }
+                    $scope.currentSlideIndex = previous;
+                    $scope.currentPagerItemIndex = previous - 1;
+
+                    setTransform(0 - ($scope.currentSlideIndex * _slideWidth));
                 }
-                $scope.options.currentItemIndex = previous;
 
-                setTransform(0 - ($scope.options.currentItemIndex * _slideWidth));
+                
             }
 
             function setTransform(offSet: number) {
+                $scope.transitionEnabled = true;
                 $scope.transform = `translateX(${offSet}px)`;
             }
         }
     }
 
     interface ICarouselScope extends ng.IScope {
+        currentPagerItemIndex: number;
+        currentSlideIndex: number;
         onNextClick: () => void;
         onPagerItemClick: (index: number) => void;
         onPreviousClick: () => void;
         options: ICarouselOptions;
+        slides: Array<ICarouselItem>;
         slidesWidth: string;
         transform: string;
+        transitionEnabled: boolean;
     }
 
     interface ICarouselOptions {
-        currentItemIndex: number;
         items: Array<ICarouselItem>;
     }
 
@@ -93,13 +131,16 @@ namespace poc {
     }
 
     function getStubOptions(): ICarouselOptions {
+        const items = [
+                { title: 'title 1', background: 'url(/ng1/poc/carousel/img1_small.png)' },
+                { title: 'title 2', background: 'url(/ng1/poc/carousel/img2_small.png)' },
+                { title: 'title 3', background: 'url(/ng1/poc/carousel/img3_small.png)' }
+            ];
+
+        
+        
         const options: ICarouselOptions = {
-            currentItemIndex: 0,
-            items: [
-                { title: 'title 1', background: 'url(/App/Core/Directives/Carousel/img1_small.png)' },
-                { title: 'title 2', background: 'url(/App/Core/Directives/Carousel/img2_small.png)' },
-                { title: 'title 3', background: 'url(/App/Core/Directives/Carousel/img3_small.png)' }
-            ]
+            items: items
         }
         return options;
     }
