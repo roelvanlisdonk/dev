32886a9fef1a1ea105021a8a23be009886dd77b1
Roel van Lisdonk <roelvanlisdonk@live.com>
2017-06-12 09:52:57 +0200

    x

--- commands/zvdz.git.ps1
+++ commands/zvdz.git.ps1
@@ -35,9 +35,6 @@ npm install
 & $MsBuild "$ZvdZOSolutionPath" /t:Clean /p:Configuration="$Configuration" /verbosity:minimal
 & $MsBuild "$ZvdZOSolutionPath" /t:Build /p:Configuration="$Configuration" /verbosity:minimal
 
-
-
-
 ####################################################  Deployment #########################################################################
 Clear-Host
 $MsBuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
