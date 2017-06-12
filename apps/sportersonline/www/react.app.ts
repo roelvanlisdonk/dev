fd1c8975cbe46597d0f7978274f473952c8615b6
Roel van Lisdonk <roelvanlisdonk@live.com>
2017-06-12 09:58:16 +0200

    x

--- apps/sportersonline/www/react.app.ts
+++ apps/sportersonline/www/react.app.ts
@@ -9,7 +9,7 @@ class Hello extends React.Component<any, any> {
     return React.createElement('div', null, `Hello ${this.props.toWhat}`);
   }
 }
-
+//
 ReactDOM.render(
   React.createElement(Hello, {toWhat: 'World'}, null),
   document.getElementById('root')
